6dfafc3961ff407f820951ca7bd8ed11ff697427
kenbouska <kenbouska@gmail.com>
2025-02-19 14:53:32 -0600

    v6

--- LoopingSamples/LoopingSamples.cpp
+++ LoopingSamples/LoopingSamples.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main()
 {
-    for (int x=1; x<=12; x++)
+    int x = 1;
+    while (x<=10)
     {
-        cout <<"6x"<<x<<"="<<(6*x)<<endl; //"6x" puts the 6 x in front of number displayed (6 x 1), x is the number (1-12), (6*x) is the math operation, endl ends line
+        cout << x << endl;
+        x++;
     }
 }
 
